e6f821e3167759adb49733c2034cd8bf848567ba
Harsh Rawat <harshrawat66@gmail.com>
2023-01-04 16:27:11 +0530

    ADD: prisma connector and shutdown configurations. (#3)
    * WIP: prisma connector.
    
    * ADD: prisma connector and shutdown configurations.
    
    Co-authored-by: Harsh Rawat <harsh.vorphy@gmail.com>

--- apps/apis/src/users/services/user.ts
+++ apps/apis/src/users/services/user.ts
@@ -1,10 +1,12 @@
+import { PrismaConnector } from '@app/core/prisma';
 import { Injectable } from '@nestjs/common';
 import { TestDto } from '../dtos/test';
 
 @Injectable()
 export class UserService {
+  constructor(private prismaClient: PrismaConnector) {}
   async getHello(params: TestDto): Promise<void> {
-    console.log(params);
+    console.log(await this.prismaClient.user.findMany());
     // await new Promise((r) => setTimeout(r, 2000));
   }
 }
